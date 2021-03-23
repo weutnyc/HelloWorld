495ce0136b6132f99bb053fe69a91327869e140a
Anton Fadeev <weutnyc@gmail.com>
2021-03-23 11:09:40 +0300

    created project

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Anton on 23.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
